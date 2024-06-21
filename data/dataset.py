5f3e80c9087a76d2f32b8925bb304e06d4f40719
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-06-21 17:39:57 +0200

    more set up

---
+++ data/dataset.py
@@ -0,0 +1,56 @@
+import os
+import torch
+import numpy as np
+from utils import image_to_patches, np_to_tensor
+import cv2
+from sklearn.model_selection import train_test_split
+from PIL import Image
+from glob import glob
+
+
+class ImageDataset(torch.utils.data.Dataset):
+    # dataset class that deals with loading the data and making it available by index.
+
+    def __init__(self, is_train, device, use_patches=True, resize_to=(400, 400)):
+        self.is_train = is_train
+        self.device = device
+        self.use_patches = use_patches
+        self.resize_to=resize_to
+        self.x, self.y, self.n_samples = None, None, None
+        self._load_data()
+
+    def _load_data(self):  # not very scalable, but good enough for now
+        images = load_all_from_path(os.path.join(self.data_dir, 'images'))
+        masks = load_all_from_path(os.path.join(self.data_dir, 'groundtruth'))
+
+        # Split into training and validation sets
+        train_images, val_images, train_masks, val_masks = train_test_split(
+            images, masks, test_size=self.split_ratio, random_state=self.random_state
+        )
+
+        self.x = train_images if self.is_train else val_images
+        self.y = train_masks if self.is_train else val_masks
+        if self.use_patches:  # split each image into patches
+            self.x, self.y = image_to_patches(self.x, self.y)
+        elif self.resize_to != (self.x.shape[1], self.x.shape[2]):  # resize images
+            self.x = np.stack([cv2.resize(img, dsize=self.resize_to) for img in self.x], 0)
+            self.y = np.stack([cv2.resize(mask, dsize=self.resize_to) for mask in self.y], 0)
+        self.x = np.moveaxis(self.x, -1, 1)  # pytorch works with CHW format instead of HWC
+        self.n_samples = len(self.x)
+
+    def _preprocess(self, x, y):
+        # to keep things simple we will not apply transformations to each sample,
+        # but it would be a very good idea to look into preprocessing
+        return x, y
+
+    def __getitem__(self, item):
+        return self._preprocess(np_to_tensor(self.x[item], self.device), np_to_tensor(self.y[[item]], self.device))
+
+    def __len__(self):
+        return self.n_samples
+
+
+def load_all_from_path(path):
+    # loads all HxW .pngs contained in path as a 4D np.array of shape (n_images, H, W, 3)
+    # images are loaded as floats with values in the interval [0., 1.]
+    return np.stack([np.array(Image.open(f)) for f in sorted(glob(path + '/*.png'))]).astype(np.float32) / 255.
---
+++ utils.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# TODO: Optimize the parameters?
+PATCH_SIZE = 16  # pixels per side of square patches
+VAL_SIZE = 10  # size of the validation set (number of images)
+CUTOFF = 0.25  # minimum average brightness for a mask patch to be classified as containing road
+
+
+def image_to_patches(images, masks=None):
+    # takes in a 4D np.array containing images and (optionally) a 4D np.array containing the segmentation masks
+    # returns a 4D np.array with an ordered sequence of patches extracted from the image and (optionally) a np.array containing labels
+    n_images = images.shape[0]  # number of images
+    h, w = images.shape[1:3]  # shape of images
+    assert (h % PATCH_SIZE) + (w % PATCH_SIZE) == 0  # make sure images can be patched exactly
+
+    images = images[:,:,:,:3]
+
+    h_patches = h // PATCH_SIZE
+    w_patches = w // PATCH_SIZE
+
+    patches = images.reshape((n_images, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE, -1))
+    patches = np.moveaxis(patches, 2, 3)
+    patches = patches.reshape(-1, PATCH_SIZE, PATCH_SIZE, 3)
+    if masks is None:
+        return patches
+
+    masks = masks.reshape((n_images, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE, -1))
+    masks = np.moveaxis(masks, 2, 3)
+    labels = np.mean(masks, (-1, -2, -3)) > CUTOFF  # compute labels
+    labels = labels.reshape(-1).astype(np.float32)
+    return patches, labels
+
+
+def show_patched_image(patches, labels, h_patches=25, w_patches=25):
+    # reorders a set of patches in their original 2D shape and visualizes them
+    fig, axs = plt.subplots(h_patches, w_patches, figsize=(18.5, 18.5))
+    for i, (p, l) in enumerate(zip(patches, labels)):
+        # the np.maximum operation paints patches labeled as road red
+        axs[i // w_patches, i % w_patches].imshow(np.maximum(p, np.array([l.item(), 0., 0.])))
+        axs[i // w_patches, i % w_patches].set_axis_off()
+    plt.show()
+
+def np_to_tensor(x, device):
+    # allocates tensors from np.arrays
+    if device == 'cpu':
+        return torch.from_numpy(x).cpu()
+    else:
+        return torch.from_numpy(x).contiguous().pin_memory().to(device=device, non_blocking=True)
+
+
