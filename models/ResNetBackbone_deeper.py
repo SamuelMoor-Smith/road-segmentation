10e59d7ff1d0dd90f95ab8487100e0585a083945
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-06-30 16:20:04 +0200

    Deeper ResNet (1 layer more)

---
+++ models/ResNetBackbone_deeper.py
@@ -0,0 +1,132 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+# follows https://arxiv.org/pdf/1711.10684
+
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Block, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.norm = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+        self.residual_connection = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        residual = self.residual_connection(x)
+        x = self.conv1(x)
+        x = self.norm(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+
+        x += residual
+
+        return x
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, strides=None):
+        super(ResBlock, self).__init__()
+
+        if strides is None:
+            strides = [1, 1]  # Only the bridge has a different stride (See table page 3 of paper)
+        self.norm1 = nn.BatchNorm2d(in_channels)
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=strides[0], padding=1)
+        self.norm2 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=strides[1], padding=1)
+
+        self.residual_connection = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=strides[0]),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        residual = self.residual_connection(x)
+        x = self.norm1(x)
+        x = self.relu(x)
+        x = self.conv1(x)
+        x = self.norm2(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+
+        x += residual
+
+        return x
+
+
+class ResNetEncoder(nn.Module):
+    def __init__(self):
+        super(ResNetEncoder, self).__init__()
+
+        resnet = models.resnet34(pretrained=True)
+
+        self.block1 = Block(3, 64)
+        self.block2 = resnet.layer2
+        self.block3 = resnet.layer3
+        self.block4 = resnet.layer4
+
+    def forward(self, x):
+        x1 = self.block1(x)
+        x2 = self.block2(x1)
+        x3 = self.block3(x2)
+        x4 = self.block4(x3)
+
+        return [x1, x2, x3, x4]
+
+
+class ResNetDecoder(nn.Module):
+    def __init__(self):
+        super(ResNetDecoder, self).__init__()
+
+        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.block1 = ResBlock(1024, 512)
+        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.block2 = ResBlock(512, 256)
+        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.block3 = ResBlock(256, 128)
+        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.block4 = ResBlock(128, 64)
+
+    def forward(self, x, encoder_features):
+        x = self.upconv1(x)
+        x = torch.cat([x, encoder_features[3]], dim=1)
+        x = self.block1(x)
+
+        x = self.upconv2(x)
+        x = torch.cat([x, encoder_features[2]], dim=1)
+        x = self.block2(x)
+
+        x = self.upconv3(x)
+        x = torch.cat([x, encoder_features[1]], dim=1)
+        x = self.block3(x)
+
+        x = self.upconv4(x)
+        x = torch.cat([x, encoder_features[0]], dim=1)
+        x = self.block4(x)
+
+        return x
+
+
+class ResNetBackbone(nn.Module):
+    def __init__(self):
+        super(ResNetBackbone, self).__init__()
+
+        self.encoder = ResNetEncoder()
+        self.bridge = ResBlock(512, 1024, strides=[2, 1])
+        self.decoder = ResNetDecoder()
+        self.projection = nn.Sequential(nn.Conv2d(64, 1, kernel_size=1), nn.Sigmoid())
+
+    def forward(self, x):
+        encoder_features = self.encoder(x)
+        x = self.bridge(encoder_features[3])
+        x = self.decoder(x, encoder_features)
+        x = self.projection(x)
+
+        return x
