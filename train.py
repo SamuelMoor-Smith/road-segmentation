9f9ca462acf78644a8f456a458547db2bc662c5d
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-06-30 12:11:46 +0200

    Basic ResUNet Architecture works

--- train.py
+++ train.py
@@ -2,18 +2,19 @@ import torch
 from torch import nn
 from data.dataset import ImageDataset
 from models.UNet_provided import UNet
+from models.ResNetBackbone import ResNetBackbone
 from utils import train, accuracy_fn, patch_accuracy_fn
 
 data_dir = '/Users/sebastian/University/Master/second_term/cil/road-segmentation/data/training'
 
 device = 'cpu'#torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-train_dataset = ImageDataset(data_dir, True, device, use_patches=False, resize_to=(384, 384))
-val_dataset = ImageDataset(data_dir, False, device, use_patches=False, resize_to=(384, 384))
+train_dataset = ImageDataset(data_dir, True, device, use_patches=False, resize_to=(224, 224))
+val_dataset = ImageDataset(data_dir, False, device, use_patches=False, resize_to=(224, 224))
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
 val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=True)
 
-model = UNet().to(device)
+model = ResNetBackbone().to(device)
 loss_fn = nn.BCELoss()
 metric_fns = {'acc': accuracy_fn, 'patch_acc': patch_accuracy_fn}
 optimizer = torch.optim.Adam(model.parameters())
