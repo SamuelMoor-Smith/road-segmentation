c294a683400c09e157548c69e018ccd63851c9d2
SamuelMoor-Smith <50902296+SamuelMoor-Smith@users.noreply.github.com>
2024-07-16 16:08:23 +0200

    Update augment.py

--- preprocess/augment.py
+++ preprocess/augment.py
@@ -1,14 +1,28 @@
-import albumentations as A
+# import albumentations as A
 
 
-def affine():
-    return A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.VerticalFlip(p=0.5),
-        A.RandomRotate90(p=0.5),
-        A.Transpose(p=0.5),
-    ], p=0.75)
+# def affine():
+#     return A.Compose([
+#         A.HorizontalFlip(p=0.5),
+#         A.VerticalFlip(p=0.5),
+#         A.RandomRotate90(p=0.5),
+#         A.Transpose(p=0.5),
+#     ], p=0.75)
 
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+import random
 
+def apply_transforms(image, mask, size=(384, 384), augment_probability=0.5):
 
+    # Apply random affine transformations
+    if random.random() < augment_probability:
+        angle = random.uniform(-10, 10)  # degrees
+        translate = [random.uniform(-image.size(2) * 0.2, image.size(2) * 0.2), random.uniform(-image.size(1) * 0.2, image.size(1) * 0.2)]
+        scale = random.uniform(0.9, 1.1)
+        shear = random.uniform(-10, 10)
 
+        image = TF.affine(image, angle=angle, translate=translate, scale=scale, shear=shear)
+        mask = TF.affine(mask, angle=angle, translate=translate, scale=scale, shear=shear)
+
+    return image, mask
